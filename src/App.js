4a31195adbc0d505e979c055cee643d0b6c173b6
Ketan K <32794827+KetanKeshav@users.noreply.github.com>
2020-07-08 14:26:41 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+import TaxForm from "./components/TaxForm";
+import TaxPaid from "./components/TaxPaid";
+import SelectPolicies from "./components/SelectPolicies";
+import PolicyDetails from "./components/PolicyDetails";
+import BookPolicy from "./components/BookPolicy";
+import Confirmation from "./components/Confirmation";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
+      <Router>
+        <div>
+          <Route exact path="/" exact component={Home} />
+          <Route path="/taxform" exact component={TaxForm} />
+          <Route path="/taxpaid" exact component={TaxPaid} />
+          <Route path="/selectpolicies" exact component={SelectPolicies} />
+          <Route path="/policydetails" exact component={PolicyDetails} />
+          <Route path="/bookpolicy" exact component={BookPolicy} />
+          <Route path="/confirmation" exact component={Confirmation} />
+        </div>
+      </Router>
+    );
   }
 }
 
-export default App
+export default App;
